0806e552ee9d846194e650809540a0b2c0f8773e
Jacob Lu <jacoblu23@gmail.com>
2018-04-18 20:02:10 -0700

    created main activity file

---
+++ app/src/main/java/org/colourhood/instamessage/MainActivity.kt
@@ -0,0 +1,13 @@
+package org.colourhood.instamessage
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.message_view)
+
+    }
+}
