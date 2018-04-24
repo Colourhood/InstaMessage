ac7231f4c832b421cfa2cd44803183196bb22f37
Jong Ho Lee <jongho2@cs.washington.edu>
2018-04-23 19:09:59 -0700

    create recycler view for chat list page in the main I have one button to direct it to my activity that I implemented my code

--- app/src/main/java/org/colourhood/instamessage/MainActivity.kt
+++ app/src/main/java/org/colourhood/instamessage/MainActivity.kt
@@ -1,12 +1,25 @@
 package org.colourhood.instamessage
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        var b = findViewById<Button>(R.id.button) as Button
+
+
+        b.setOnClickListener{
+
+            val intentChatList = Intent(this, ChatListView::class.java)
+            startActivity(intentChatList);
+
+        }
     }
 }
